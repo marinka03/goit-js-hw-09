6b5b510b3e07bf9af5406390d2820c25a02997b6
Marina Chernyakina <chernyakinamarina@gmail.com>
2023-04-28 15:25:59 +0300

    add-third-task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
-function createPromise(position, delay) {
+import Notiflix from 'notiflix';
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", onSubmit);
+
+function createPromise(position, currentDelay, step) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  let delay = currentDelay + step * position;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+      if (shouldResolve) {
+        resolve ({position, delay})
+      } else {
+        reject ({position, delay})
+      }
+    }, delay);
+  });
+
+  promise.then(({position, delay}) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position + 1} in ${delay} ms`);
+  })
+  .catch(({position, delay}) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position + 1} in ${delay} ms`);
+  });
+}
+
+function onSubmit(evt){
+  evt.preventDefault();
+  const arrayInput = [...form].filter((item)=>{
+    return item.type === "number"
+  });
+
+  const obj = {};
+
+  for (const item of arrayInput) {
+    obj[item.name] = item.value;
+  }
+    
+  const {delay, step, amount} = obj;
+  let currentDelay = +delay;
+  let currentStep = +step;
+  
+  for(let i = 0; i < amount; i++){
+    createPromise(i, currentDelay, currentStep);
   }
 }
